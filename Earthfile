75263a8c356611a446c5147cd6938dd1ad343082
Prad Nukala <prad@sonr.io>
2023-11-21 01:48:29 -0500

    Update dependencies and generate code

--- Earthfile
+++ Earthfile
@@ -2,36 +2,56 @@ VERSION 0.7
 PROJECT sonrhq/sonr-testnet-0
 
 FROM golang:1.21-alpine3.17
-
 RUN apk add --update --no-cache \
     bash \
     bash-completion \
     binutils \
     ca-certificates \
+    clang-extra-tools \
     coreutils \
     curl \
     findutils \
     g++ \
     git \
     grep \
+    jq \
     less \
     make \
+    nodejs \
+    npm \
     openssl \
     util-linux
 
 WORKDIR /service
 
-# deps downloads and caches all dependencies for earthly. When called directly,
-# go.mod and go.sum will be updated locally.
-deps:
-	FROM +base
-    RUN curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.1
+# gomod - downloads and caches all dependencies for earthly. go.mod and go.sum will be updated locally.
+gomod:
+    FROM +base
     COPY go.mod go.sum ./
     RUN go mod download
     SAVE ARTIFACT go.mod AS LOCAL go.mod
     SAVE ARTIFACT go.sum AS LOCAL go.sum
 
+
+# deps - downloads and caches all dependencies for earthly. go.mod and go.sum will be updated locally.
+deps:
+    FROM +base
+    RUN npm install -g swagger-combine
+    RUN npm install @bufbuild/buf
+    FROM ghcr.io/cosmos/proto-builder:0.14.0
+    RUN go install cosmossdk.io/orm/cmd/protoc-gen-go-cosmos-orm@latest
+	RUN go install cosmossdk.io/orm/cmd/protoc-gen-go-cosmos-orm-proto@latest
+    SAVE IMAGE deps
+
+# generate - generates all code from proto files
+generate:
+    FROM +deps
+    COPY . .
+    RUN sh ./scripts/protocgen.sh
+    SAVE ARTIFACT sonrhq/service AS LOCAL api
+    SAVE ARTIFACT proto AS LOCAL proto
+
+# test - runs all tests
 test:
-	FROM +deps
-	COPY . .
+    LOCALLY
 	RUN go test -v ./...
--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-echo "Generating gogo proto code"
+
 cd proto
+echo "Generating gogo proto code"
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    # this regex checks if a proto file has its go_package set to github.com/sonrhq/service/api/...
-    # gogo proto files SHOULD ONLY be generated if this is false
-    # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
     if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*github.com/sonrhq/service/api' "$file" | grep -q ':0$'; then
       buf generate --template buf.gen.gogo.yaml $file
     fi
@@ -17,11 +15,30 @@ for dir in $proto_dirs; do
 done
 
 echo "Generating pulsar proto code"
-buf generate --template buf.gen.pulsar.yaml
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*github.com/sonrhq/service/api' "$file" | grep -q ':0$'; then
+      buf generate --template buf.gen.pulsar.yaml
+    fi
+  done
+done
+
+echo "Generating orm code"
+proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+  for file in $(find "${dir}" -maxdepth 1 -name 'state.proto'); do
+      buf generate --template buf.gen.proto.yaml
+      buf generate --template buf.gen.yaml
+  done
+done
 
 cd ..
 
-cp -r github.com/sonrhq/service/* ./
-rm -rf api && mkdir api
-mv sonrhq/service/* ./api
-rm -rf github.com sonrhq
+# Find and delete uncompatible proto implementations
+find . -name 'state_query.pulsar.go' -delete
+find . -name 'state.pulsar.go' -delete
+find . -name 'module.pb.go' -delete
+find . -name 'query.pb.go' -delete
+find . -name 'tx.pb.go' -delete
+find . -name 'types.pb.go' -delete
