457b00f6d79d1ce464b502e02bc18066aa5a9f0b
Prad Nukala <prad@sonr.io>
2023-12-16 23:47:32 -0500

    fix: autocli commands

--- module/autocli.go
+++ module/autocli.go
@@ -2,6 +2,7 @@ package module
 
 import (
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
+
 	servicev1 "github.com/sonrhq/service/api/v1"
 )
 
@@ -12,14 +13,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 			Service: servicev1.Query_ServiceDesc.ServiceName,
 			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
 				{
-					RpcMethod: "Counter",
-					Use:       "counter [address]",
-					Short:     "Get the current value of the counter for an address",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
-						{ProtoField: "address"},
-					},
-				},
-				{
 					RpcMethod: "Params",
 					Use:       "params",
 					Short:     "Get the current module parameters",
@@ -29,14 +22,6 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 		Tx: &autocliv1.ServiceCommandDescriptor{
 			Service: servicev1.Msg_ServiceDesc.ServiceName,
 			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
-				{
-					RpcMethod: "IncrementCounter",
-					Use:       "counter [sender]",
-					Short:     "Increments the counter by 1 for the sender",
-					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
-						{ProtoField: "sender"},
-					},
-				},
 				{
 					RpcMethod: "UpdateParams",
 					Skip:      true, // This is a authority gated tx, so we skip it.
